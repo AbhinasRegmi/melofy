5215dfafc8a933da248c218286293dc9aa5fc7a5
Abhinas Regmi <77847543+AbhinasRegmi@users.noreply.github.com>
2023-07-04 16:13:27 +0000

    Added Cors Middleware

--- backend/src/melofy/core/config.py
+++ backend/src/melofy/core/config.py
@@ -2,6 +2,7 @@
 All project settings are configured here.
 """
 
+from typing import List
 from functools import lru_cache
 
 from pydantic import BaseSettings
@@ -14,6 +15,11 @@ class _Setting(BaseSettings):
 
     PROJECT_NAME: str = "melofy"
     PROJECT_VERSION: str = "1.0.0"
+
+    CORS_ALLOWED_ORIGINS: List[str] = [
+        "https://abhinasregmi.com.np",
+        "http://localhost:5500",
+    ]
 
 
 @lru_cache
--- backend/src/melofy/main.py
+++ backend/src/melofy/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from melofy.core.config import settings
 
@@ -6,4 +7,12 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
     redoc_url=''
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
 )
